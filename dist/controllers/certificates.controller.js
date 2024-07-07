0de704189313442f851381e5e96fe8e58499b3de
ImJustNon <Im_Just_Non@hotmail.com>
2024-07-08 02:01:59 +0700

    Feat : Fix build command 3

---
+++ dist/controllers/certificates.controller.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getCertificateAll = getCertificateAll;
+exports.getCertificateById = getCertificateById;
+exports.getCertificateByCategory = getCertificateByCategory;
+const certificates_data_1 = require("../data/certificates/certificates.data");
+function getCertificateAll(req, res) {
+    res.json((0, certificates_data_1.getCertificates)());
+    return;
+}
+function getCertificateById(req, res) {
+    var _a;
+    const { id } = req.params;
+    const allCertificates = (0, certificates_data_1.getCertificates)();
+    const allCertificatesArray = Object.values(allCertificates).flat();
+    const filterCertificateDataById = allCertificatesArray.filter((cert) => cert.id === id);
+    res.json({
+        status: "OK",
+        message: "This is certificate data",
+        data: (_a = filterCertificateDataById[0]) !== null && _a !== void 0 ? _a : {},
+    });
+    return;
+}
+function getCertificateByCategory(req, res) {
+    var _a;
+    const category = req.params.category;
+    const allCertificates = (0, certificates_data_1.getCertificates)();
+    res.json({
+        status: "OK",
+        message: "This is certificate data",
+        data: (_a = allCertificates[category]) !== null && _a !== void 0 ? _a : [],
+    });
+    return;
+}
---
+++ dist/data/activities/activities.data.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getActivities = getActivities;
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+function getActivities() {
+    let activities = {};
+    const scanForActivityJsonFile = fs_1.default.readdirSync(path_1.default.join(__dirname, "./json")).filter(filename => filename.endsWith(".json"));
+    for (const filename of scanForActivityJsonFile) {
+        const fileData = fs_1.default.readFileSync(path_1.default.join(__dirname, "./json", filename), {
+            encoding: "utf8",
+        });
+        const parseJsonData = JSON.parse(fileData);
+        activities = Object.assign(Object.assign({}, parseJsonData), activities);
+    }
+    return activities;
+}
